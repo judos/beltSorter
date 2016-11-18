e4a27a33704f4e1b834a27e345410f92e8d9960f
judos <judos@gmx.ch>
2016-11-18 22:29:39 +0100

    rewrite entityId function

--- source/libs/entityId.lua
+++ source/libs/entityId.lua
@@ -12,33 +12,36 @@ function entityOfId(id,searchName)
 	end
 	local position = split(id,"_")
 	local surfaceIndex = tonumber(position[1])
-	local point = {tonumber(position[2]),tonumber(position[3])}
-	local entities = surfaceWithIndex(surfaceIndex).find_entities{point,point}
+	local point = {x=tonumber(position[2]),y=tonumber(position[3])}
+	local entities = surfaceWithIndex(surfaceIndex).find_entities_filtered{position=point}
 	if entities == nil or #entities ==0 then
 		err("No entity found for id provided: "..id)
 		return nil
+	elseif #entities == 1 then
+		return entities[1]
 	end
-	if #entities > 1 then
-		if searchName then
-			local result
-			local found = 0
-			for k,entity in pairs(entities) do
-				if entity.name:find(searchName) then
-					result = entity
-					found = found + 1
-				end
-			end
-			if found ~= 1 then
-				warn("entitiyOfId found "..tostring(found).." entities with name "..searchName.." for id "..id)
-			end
-			if result then return result end
+	
+	if not searchName then
+		warn("Using at random the first entity")
+		return entities[1]
+	end
+	
+	warn("Multiple entities found for id provided: "..id)
+	local result = nil
+	local found = 0
+	for k,entity in pairs(entities) do
+		if entity.name == searchName then
+			return entity
 		end
-		warn("Multiple entities found for id provided: "..id.." using entity with name: "..entities[1].name)
-		for k,entity in pairs(entities) do
-			info("available: "..tostring(k).." = "..entity.name)
+		if entity.name:find(searchName) then
+			result = entity
+			found = found + 1
 		end
 	end
-	return entities[1]
+	if found ~= 1 then
+		warn("entitiyOfId found "..tostring(found).." entities with name "..searchName.." for id "..id)
+	end
+	return result
 end
 
 
