7e7631576617c99b0fed36abe15af0447a0dc130
judos <judos@gmx.ch>
2017-06-02 23:51:20 +0200

    add special filter item to item group for visibility in the selection dialog

--- source/prototypes/everything-else-filter-item.lua
+++ source/prototypes/everything-else-filter-item.lua
@@ -2,11 +2,25 @@
 -- Item
 data:extend({
 	{
+		type = "item-group",
+		name = "beltSorter-filters",
+		order = "g",
+		icon_size = 64,
+		icon = "__beltSorter__/graphics/item-groups/belt-sorter-filters.png",
+	},
+	{
+		type = "item-subgroup",
+		name = "beltSorter-filters",
+		group = "beltSorter-filters",
+		order = "a"
+	},
+	{
 		type = "item",
 		name = "belt-sorter-everythingelse",
 		order = "z[belt-sprter-everythingelse]",
 		icon = "__beltSorter__/graphics/icons/belt-sorter-everythingelse.png",
+		subgroup = "beltSorter-filters",
 		stack_size = 1,
-		flags = {"goes-to-main-inventory", "hidden"}
+		flags = {"goes-to-main-inventory"}
 	}
 })
