aa644726e9e2a88f64de47a45c624352d9721021
judos <judos@gmx.ch>
2017-12-26 13:08:30 +0100

    v0.5.2

--- source/constants.lua
+++ source/constants.lua
@@ -1,9 +1,9 @@
 
-modVersion = "0.3.7" --version of control data, only updated when control.lua changes
+modVersion = "0.3.7" --version of control data, only updated when control.lua data handling changes in a major way
 modName = "beltSorter" -- required prefix for all ui name components which can be clicked
 fullModName = "beltSorter" -- required for logging and prototypes
 
 
 libLog.debug_master = true
---libLog.testing = false -- enables player printing of every log, sets log level to info
-libLog.debug_level = 1  -- 1=info 2=warn 3=error
+--libLog.testing = true -- enables player printing of every log, sets log level to info
+--libLog.debug_level = 1  -- 1=info 2=warn 3=error
