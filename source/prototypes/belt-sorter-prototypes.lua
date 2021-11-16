db91d6f60da0aed835e29ca285a35d1968cad9d3
snouz <snooz-x@hotmail.com>
2021-11-16 11:35:11 +0100

    New way to render sorters (5 bases, 1 glowing "on" light, 1 shadow)

--- source/prototypes/belt-sorter-prototypes.lua
+++ source/prototypes/belt-sorter-prototypes.lua
@@ -6,19 +6,53 @@ local noPicture = {
   shift = {0, 0}
 }
 
-local function picture(lvl,mode)
-  return {
-    filename="__beltSorter__/graphics/entity/belt-sorter"..lvl.."-"..mode..".png",
-    width = 64,
-    height = 64,
-    shift = {0, 0},
-    hr_version = {
-      filename="__beltSorter__/graphics/entity/belt-sorter"..lvl.."-hr-"..mode..".png",
-      width = 128,
-      height = 128,
-      scale = 0.5,
+local function picture(lvl, mode_on)
+  layering = {
+    layers = {
+      {
+        filename = "__beltSorter__/graphics/entity/belt-sorter-" .. lvl .. "-base.png",
+        width = 64,
+        height = 64,
+        hr_version = {
+          filename = "__beltSorter__/graphics/entity/hr-belt-sorter-" .. lvl .. "-base.png",
+          width = 128,
+          height = 128,
+          scale = 0.5,
+        }
+      },
+      {
+        filename = "__beltSorter__/graphics/entity/belt-sorter-shadow.png",
+        width = 64,
+        height = 64,
+        draw_as_shadow = true,
+        hr_version = {
+          filename = "__beltSorter__/graphics/entity/hr-belt-sorter-shadow.png",
+          width = 128,
+          height = 128,
+          scale = 0.5,
+          draw_as_shadow = true,
+        }
+      },
     }
   }
+
+  if mode_on then
+    table.insert(layering.layers, {
+        filename = "__beltSorter__/graphics/entity/belt-sorter-light-on.png",
+        width = 64,
+        height = 64,
+        draw_as_glow = true,
+        hr_version = {
+          filename = "__beltSorter__/graphics/entity/hr-belt-sorter-light-on.png",
+          width = 128,
+          height = 128,
+          scale = 0.5,
+          draw_as_glow = true,
+        }
+      }
+    )
+  end
+  return layering
 end
 
 function createBeltSorterPrototype(i, energy)
@@ -29,8 +63,8 @@ function createBeltSorterPrototype(i, energy)
     icon = "__beltSorter__/graphics/icons/belt-sorter"..i..".png",
     energy_usage_per_tick = tostring(energy).."W",
     light = {intensity = 0, size = 0},
-    picture_off = picture(i,"off"),
-    picture_on = picture(i,"on")
+    picture_off = picture(i, false),
+    picture_on = picture(i, true)
   })
   beltSorter.minable.result = "belt-sorter"..i
   beltSorter.circuit_connector_sprites = nil
@@ -48,7 +82,7 @@ function createBeltSorterPrototype(i, energy)
     light = {intensity = 0, size = 0},
     flags = {"placeable-off-grid", "not-repairable", "not-on-map"},
     picture_off= noPicture,
-    picture_on = picture(i,"on")
+    picture_on = picture(i, true),
   })
   beltSorterLamp.circuit_connector_sprites = nil
 
--- source/prototypes/belt-sorter-technologies.lua
+++ source/prototypes/belt-sorter-technologies.lua
@@ -6,7 +6,7 @@ data:extend({
     type = "technology",
     name = "belt-sorter1",
     icon = "__beltSorter__/graphics/technology/belt-sorter1.png",
-    icon_size = 128,
+    icon_size = 256,
     prerequisites = {"electronics"},
     effects = {},
     unit = {
@@ -22,8 +22,8 @@ data:extend({
     type = "technology",
     name = "belt-sorter2",
     icon = "__beltSorter__/graphics/technology/belt-sorter2.png",
-    icon_size = 128,
-    prerequisites = {"belt-sorter1", "advanced-electronics" },
+    icon_size = 256,
+    prerequisites = {"belt-sorter1", "advanced-electronics"},
     effects = {},
     unit = {
       count = 20,
@@ -39,12 +39,16 @@ data:extend({
     type = "technology",
     name = "belt-sorter3",
     icon = "__beltSorter__/graphics/technology/belt-sorter3.png",
-    icon_size = 128,
+    icon_size = 256,
     prerequisites = {"belt-sorter2", "advanced-electronics-2", "production-science-pack"},
     effects = {},
     unit = {
-      count = 20,
-      ingredients = {{"automation-science-pack", 3},{"logistic-science-pack", 6},{"production-science-pack", 3}},
+      count = 60,
+      ingredients = {
+      	{"automation-science-pack", 1},
+      	{"logistic-science-pack", 2},
+      	{"production-science-pack", 1}
+      },
       time = 15
     },
     order = "_belt-sorter3"
