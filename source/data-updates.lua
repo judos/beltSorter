1a8d5b9df36fc3941822a6f966791092f2f48a22
snouz <snooz-x@hotmail.com>
2021-12-10 17:47:19 +0100

    Added 9 mod compatibilities

--- source/data-updates.lua
+++ source/data-updates.lua
@@ -5,8 +5,8 @@ require("prototypes.belt-sorter-prototypes")
 local IsBeltSorter4_active = false
 local IsBeltSorter5_active = false
 
-local mods_with_4belts = {}
-local mods_with_5belts = {"boblogistics", "UltimateBelts", "Krastorio2"}
+local mods_with_4belts = {"one-more-tier", "BetterBelts"}
+local mods_with_5belts = {"boblogistics", "UltimateBelts", "Krastorio2", "modmashsplinterlogistics", "5dim_transport", "FactorioExtended-Transport", "FactorioExtended-Plus-Transport", "iper-belt", "Hiladdar_Belts"}
 
 for _,mod in pairs(mods_with_4belts) do
   if mods[mod] then
@@ -49,11 +49,57 @@ if data.raw.tool["advanced-logistic-science-pack"] then
 end
 
 --------------------------------------------------------------------- ADAPT COMPATIBILITY -----------------------------------------------------------------------
------------------------ Bob's Logistics----------------------
+
+local function SwapColorOfSorterTier(tier, destination)
+  data.raw.item["belt-sorter" .. tier].icon = "__beltSorter__/graphics/icons/belt-sorter" .. destination .. ".png"
+  data.raw.lamp["belt-sorter" .. tier].icon = "__beltSorter__/graphics/icons/belt-sorter" .. destination .. ".png"
+  data.raw.lamp["belt-sorter" .. tier].picture_off.layers[1].filename = "__beltSorter__/graphics/entity/hr-belt-sorter-" .. destination .. "-base.png"
+  data.raw.lamp["belt-sorter-lamp" .. tier].icon = "__beltSorter__/graphics/icons/belt-sorter" .. destination .. ".png"
+  data.raw.technology["belt-sorter" .. tier].icon = "__beltSorter__/graphics/technology/belt-sorter" .. destination .. ".png"
+end
+
+----------------------- Hiladdar's Belts -----------------------
+if mods["Hiladdar_Belts"] then
+  SwapColorOfSorterTier(4, 5)
+  SwapColorOfSorterTier(5, 4)
+  if data.raw.technology["logistics-4"] then table.insert(data.raw.technology["belt-sorter4"].prerequisites, "logistics-4") end
+  if data.raw.technology["logistics-5"] then table.insert(data.raw.technology["belt-sorter5"].prerequisites, "logistics-5") end
+end
+
+----------------------- One More Tier -----------------------
+if mods["one-more-tier"] then
+  if data.raw.technology["omt-logistics-4"] then table.insert(data.raw.technology["belt-sorter4"].prerequisites, "omt-logistics-4") end
+end
+
+----------------------- Better Belts -----------------------
+if mods["BetterBelts"] then
+  SwapColorOfSorterTier(4, 5)
+  if data.raw.technology["BetterBelts_ultra-class"] then table.insert(data.raw.technology["belt-sorter4"].prerequisites, "BetterBelts_ultra-class") end
+end
+
+----------------------- FactorioExtended -----------------------
+if mods["FactorioExtended-Transport"] then
+  SwapColorOfSorterTier(4, 5)
+  SwapColorOfSorterTier(5, 4)
+  if data.raw.technology["stainless-steel-tech"] then table.insert(data.raw.technology["belt-sorter4"].prerequisites, "stainless-steel-tech") end
+  if data.raw.technology["titanium-tech"] then table.insert(data.raw.technology["belt-sorter5"].prerequisites, "titanium-tech") end
+end
+
+----------------------- FactorioExtended Plus -----------------------
+if mods["FactorioExtended-Plus-Transport"] then
+  SwapColorOfSorterTier(4, 5)
+  SwapColorOfSorterTier(5, 4)
+  if data.raw.technology["logistics-4"] then table.insert(data.raw.technology["belt-sorter4"].prerequisites, "logistics-4") end
+  if data.raw.technology["logistics-5"] then table.insert(data.raw.technology["belt-sorter5"].prerequisites, "logistics-5") end
+  for i=4,5 do
+    data.raw.item["belt-sorter" .. i].subgroup = "fb-transport"
+  end
+end
+
+----------------------- Bob's Logistics ----------------------
 if mods["boblogistics"] then
   if data.raw.technology["logistics-4"] then table.insert(data.raw.technology["belt-sorter4"].prerequisites, "logistics-4") end
   if data.raw.technology["logistics-5"] then table.insert(data.raw.technology["belt-sorter5"].prerequisites, "logistics-5") end
-
 
   for i=1,5 do
     data.raw.item["belt-sorter" .. i].subgroup = "bob-logistic-tier-" .. i
@@ -69,25 +115,37 @@ end
 
 ----------------------- Krastorio 2 ----------------------
 if mods["Krastorio2"] then
-
-  --switch graphics: 4=green, 5=purple
-  data.raw.item["belt-sorter4"].icon = "__beltSorter__/graphics/icons/belt-sorter5.png"
-  data.raw.item["belt-sorter5"].icon = "__beltSorter__/graphics/icons/belt-sorter4.png"
-
-  data.raw.lamp["belt-sorter4"].icon = "__beltSorter__/graphics/icons/belt-sorter5.png"
-  data.raw.lamp["belt-sorter5"].icon = "__beltSorter__/graphics/icons/belt-sorter4.png"
-
-  data.raw.lamp["belt-sorter4"].picture_off.layers[1].filename = "__beltSorter__/graphics/entity/hr-belt-sorter-5-base.png"
-  data.raw.lamp["belt-sorter5"].picture_off.layers[1].filename = "__beltSorter__/graphics/entity/hr-belt-sorter-4-base.png"
-
-  data.raw.lamp["belt-sorter-lamp4"].icon = "__beltSorter__/graphics/icons/belt-sorter5.png"
-  data.raw.lamp["belt-sorter-lamp5"].icon = "__beltSorter__/graphics/icons/belt-sorter4.png"
-
-  data.raw.technology["belt-sorter4"].icon = "__beltSorter__/graphics/technology/belt-sorter5.png"
-  data.raw.technology["belt-sorter5"].icon = "__beltSorter__/graphics/technology/belt-sorter4.png"
+  SwapColorOfSorterTier(4, 5)
+  SwapColorOfSorterTier(5, 4)
 
   if data.raw.technology["kr-logistic-4"] then table.insert(data.raw.technology["belt-sorter4"].prerequisites, "kr-logistic-4") end
   if data.raw.technology["kr-logistic-5"] then table.insert(data.raw.technology["belt-sorter5"].prerequisites, "kr-logistic-5") end
 end
 
---to add: one-more-tier se/AAI modmash/modmashsplinterlogistics 5dim_transport BetterBelts FactorioExtended-Transport
+----------------------- Mod Mash ----------------------
+if mods["modmashsplinterlogistics"] then
+  SwapColorOfSorterTier(4, 5)
+  SwapColorOfSorterTier(5, 4)
+
+  if data.raw.technology["logistics-4"] then table.insert(data.raw.technology["belt-sorter4"].prerequisites, "logistics-4") end
+  if data.raw.technology["logistics-5"] then table.insert(data.raw.technology["belt-sorter5"].prerequisites, "logistics-5") end
+end
+
+----------------------- 5Dim ----------------------
+if mods["5dim_transport"] then
+  data:extend({
+    {
+      type = "item-subgroup",
+      name = "5d-belt-sorters",
+      group = "transport",
+      order = "e1",
+    }
+  })
+
+  for i=1,5 do
+    data.raw.item["belt-sorter" .. i].subgroup = "5d-belt-sorters"
+  end
+
+  if data.raw.technology["logistics-4"] then table.insert(data.raw.technology["belt-sorter4"].prerequisites, "logistics-4") end
+  if data.raw.technology["logistics-5"] then table.insert(data.raw.technology["belt-sorter5"].prerequisites, "logistics-5") end
+end
