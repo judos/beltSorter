1e922526d0ab82f19bcf8a2c7b0341f4e6b62d70
snouz <snooz-x@hotmail.com>
2021-12-10 11:58:40 +0100

    removed useless hidden entity icon

--- source/prototypes/belt-sorter-config-combinator.lua
+++ source/prototypes/belt-sorter-config-combinator.lua
@@ -4,7 +4,6 @@ local noPicture = { filename = "__beltSorter__/graphics/entity/empty.png", size 
 local combinator = deepcopy(data.raw["constant-combinator"]["constant-combinator"])
 combinator.name = "belt-sorter-config-combinator"
 combinator.item_slot_count = 21
-combinator.icon = "__beltSorter__/graphics/icons/belt-sorter-config.png"
 combinator.minable = {hardness = 10, mining_time = 10, results = {}}
 combinator.order = "zzz"
 combinator.collision_box = {{0,0}, {0,0}}
@@ -16,7 +15,6 @@ data:extend({combinator})
 -- Config item (needed to be stored in blueprint)
 local combinatorItem = deepcopy(data.raw["item"]["constant-combinator"])
 combinatorItem.name = "belt-sorter-config-combinator"
-combinatorItem.icon = "__beltSorter__/graphics/icons/belt-sorter-config.png"
 combinatorItem.flags = {"hidden"}
 combinatorItem.place_result = "belt-sorter-config-combinator"
 data:extend({combinatorItem})
